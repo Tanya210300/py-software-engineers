4390af35b29c3793798897a44bacad8385d28667
Tanya <tanya210300@gmail.com>
2026-02-23 14:41:11 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+class SoftwareEngineer:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.skills = []
+
+    def learn_skill(self, skill: str) -> None:
+        self.skills.append(skill)
+
+
+engineer = SoftwareEngineer("Max")
+engineer.learn_skill("Python")
+print(engineer.name)
+print(engineer.skills)
