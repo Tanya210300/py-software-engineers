bc93caec95be26eeacf7790d5f5b97beb589080f
Tanya <tanya210300@gmail.com>
2026-02-23 15:32:19 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -37,13 +37,8 @@ class AndroidDeveloper(SoftwareEngineer):
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(
-    BackendDeveloper,
-    FrontendDeveloper,
-):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-
+class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
-        return self.create_powerful_api(), self.create_awesome_web_page()
+        self.create_powerful_api()
+        self.create_awesome_web_page()
